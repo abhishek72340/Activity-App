816f4e8590f3a1bf6659256fc064f453a5240cdd
Abhishek Singh Rana <abhisingh.72340@gmail.com>
2023-11-26 01:40:32 +0530

    feature add

--- src/Components/singleCard/SingleCard.jsx
+++ src/Components/singleCard/SingleCard.jsx
@@ -1,28 +1,43 @@
-import React from 'react'
-import './SingleCard.css';
-import stone from '../../screenshot/stone.png'
-export default function SingleCard({ card, handleChoice, flipped, gameStart, setGameStart }) {
-
+import React from "react";
+import "./SingleCard.css";
+import stone from "../../screenshot/stone.png";
+export default function SingleCard({
+  card,
+  handleChoice,
+  flipped,
+  gameStart,
+  setGameStart,
+}) {
   const handleClick = () => {
-    handleChoice(card)
-  }
+    handleChoice(card);
+  };
 
   const imageClickHandler = () => {
-    setGameStart(false)
-  }
+    setGameStart(false);
+  };
   return (
+    <div className="card">
+      <div className={flipped ? "flipped" : ""}>
+        <img
+          src={card.src}
+          alt=""
+          className="size front"
+          style={{ transform: gameStart ? "rotateY(0deg)" : "" }}
+          onClick={imageClickHandler}
+        />
 
-    <div className='card' >
-      <div className={flipped ? 'flipped' : ''}>
-
-        <img src={card.src} alt="" className='size front' style={{ transform: gameStart ? 'rotateY(0deg)' : '' }} onClick={imageClickHandler} />
-
-        <img src={stone} onClick={handleClick} alt="" className='size back' style={{ visibility: gameStart ? 'hidden' : '' }} />
-
+        <img
+          src={stone}
+          onClick={handleClick}
+          alt=""
+          className="size back"
+          style={{ visibility: gameStart ? "hidden" : "" }}
+        />
       </div>
     </div>
-
-  )
+  );
 }
 
 // quiz
+
+///Feature branch
